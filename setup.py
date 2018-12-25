1337200a4d893934376355b79983b687c6aead63
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2018-12-25 18:52:23 +0100

    fixing x3

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 setup(
     name = 'facebook-handler',
     packages = ['facebook-handler'],
-    version = '0.2',
+    version = '0.1',
     license = 'MIT',
     description = 'A python library made with selenium to control a facebook account \
                 without the use of the offical API',
     author = 'Ahmed Tounsi',
     author_email = 'ahmeddottounsi@gmail.com',
     url = 'https://github.com/ahmed-tounsi',
-    download_url = 'https://github.com/ahmed-tounsi/facebookHandler/archive/0.2.tar.gz',
+    download_url = 'https://github.com/ahmed-tounsi/facebookHandler/archive/0.1.tar.gz',
     keywords = ['facebook', 'selenium', 'facebook-bot'],
     install_requires =[
         'selenium',
