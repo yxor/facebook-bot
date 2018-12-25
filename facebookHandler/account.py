fef43fb560a171ccfe688df22d85d9890da21b38
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2018-12-25 18:40:49 +0100

    packaging

--- facebookHandler/account.py
+++ facebookHandler/account.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-from utils import *
+from .utils import getStringBetween
 
 import time
 import random
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'facebook-handler',
+    packages = ['facebook-handler'],
+    version = '0.1',
+    license = 'MIT',
+    description = 'A python library made with selenium to control a facebook account \
+                without the use of the offical API',
+    author = 'Ahmed Tounsi',
+    author_email = 'ahmeddottounsi@gmail.com',
+    url = 'https://github.com/ahmed-tounsi',
+    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords = ['facebook', 'selenium', 'facebook-bot'],
+    install_requires =[
+        'selenium',
+    ],
+    classifiers =[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
